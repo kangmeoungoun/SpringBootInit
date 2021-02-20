899770fcccc9a7dbb92d23aa6ee226ddf733a407
kangmeoungoun <kes98202@naver.com>
2021-02-20 21:09:56 +0900

    master first commit

---
+++ src/main/java/me/goldapple/springbooinit/SpringbooinitApplication.java
@@ -0,0 +1,13 @@
+package me.goldapple.springbooinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbooinitApplication.class, args);
+	}
+
+}
