4e8070aca79d69710be710a7eeddcf0b781d5ca0
kangmeoungoun <kes98202@naver.com>
2021-03-01 23:25:13 +0900

    테스트 유틸

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 #### 스프링 부트 활용
-##### 테스트
+##### 테스트 유틸
 
-![image](https://user-images.githubusercontent.com/40969203/109508576-5492be00-7ae3-11eb-9a35-8f6ba6140784.png)
+
+특정 로그 메시지가 출력이 되는지 테스트 코드로 확인하고 싶을 때
+```java
+CapturedOutput capturedOutput
+```
+
+![image](https://user-images.githubusercontent.com/40969203/109510289-2c0bc380-7ae5-11eb-8fc8-63479f99472d.png)
--- src/main/java/me/goldapple/springbooinit/sample/SampleController.java
+++ src/main/java/me/goldapple/springbooinit/sample/SampleController.java
@@ -1,16 +1,21 @@
 package me.goldapple.springbooinit.sample;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class SampleController{
+    Logger logger = LoggerFactory.getLogger(SampleController.class);
     private final SampleService sampleService;
 
     @GetMapping("/hello")
     public String hello(){
+        logger.info("holoman");
+        System.out.println("skip");
         return "hello " + sampleService.getName();
     }
 }
